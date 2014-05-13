c03b05ccb9f2dc28e7aeb2a5109177b2f50bb903
James Miltenberger <jamilt13@gmail.com>
2014-05-13 13:45:15 -0500

    updated files

--- js/game.js
+++ js/game.js
@@ -6,20 +6,24 @@
 var cards = new Array();
 
 for(var i = 0; i < 13; i++) {
-	cards.push(new card(i+2, "diamonds"));
-	cards.push(new card(i+2, "hearts"));
-	cards.push(new card(i+2, "spades"));
-	cards.push(new card(i+2, "clubs"));
+	cards.push(new card(i+2, i*4));
+	cards.push(new card(i+2, i*4+1));
+	cards.push(new card(i+2, i*4+2));
+	cards.push(new card(i+2, i*4+3));
 }
 
 //card object
-function card(value, suit) {
-	this.value = value;
-	this.suit = suit;
-	value < 10 ? this.points = value : this.points = 10;
+function card(points, number) {
+	this.number = number;
+	if(points < 11) this.points = points
+	else if(points >= 11 &&  points < 14) this.points = 10;
+	else this.points = 11;
 }
 
 function newGame() {
 	var c = confirm("Are you sure you wish to begin a new game?")
-	if(c) $("#content").empty().append("");
+	if(c) $("#content").empty().append("\
+		<h3 class='Centered'>BlackJack 21</h3>\
+		<div class='dealer'></div>\
+		<div class='player'></div>");
 }
