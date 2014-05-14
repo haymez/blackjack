8d15b88d67b0ff2e57b588c068992797396932ae
James Miltenberger <jamilt13@gmail.com>
2014-05-14 13:39:16 -0500

    Updated game logic

--- js/game.js
+++ js/game.js
@@ -7,8 +7,8 @@ var cards = new Array(); //Holds all the cards and their point values
 var deck; //remove cards from this deck as you use them
 var dealerHand = 0; //Keeps track of how many cards dealer has
 var playerHand = 0; //Keeps track of how many cards player has
-var dealerCards = new Array();
-var playerCards = new Array();
+var dealerCards = new Array(); //Holds the card objects for dealer
+var playerCards = new Array(); //Holds the card objects for player
 
 for(var i = 0; i < 13; i++) {
 	cards.push(new card(i+2, i*4));
@@ -29,29 +29,39 @@ function card(points, number) {
 
 function newGame() {
 	var c = confirm("Are you sure you wish to begin a new game?")
-	if(c) $("#content").empty().append("\
+	if(c) {
+		$("#content").empty().append("\
 		<h2 class='Centered noPad'>BlackJack</h2>\
 		\
 		<div class='dealer Centered'>\
 			<h4 class='Centered'>Dealer</h4>\
 			<div id='dealer' class='cards'></div>\
-			<div id='dealerInfo' class='game-info'></div>\
+			<div id='dealerInfo' class='game-info well'>\
+			<h3>0</h3>\
+			</div>\
 		</div>\
 		\
 		<div class='player Centered'>\
 			<h4 class='Centered'>Player</h4>\
 			<div id= 'player' class='cards'></div>\
-			<div id='playerInfo' class='game-info'></div>\
+			<div id='playerInfo' class='game-info well'></div>\
 		</div>\
 		\
 		<div class='controls Centered'>\
 			<h4 class='Centered'>Player Controls</h4>\
-			<div class='btn-group'>\
-				<button class='btn btn-lg btn-default' onclick='deal();'>Deal</button>\
-				<button class='btn btn-lg btn-default' onclick='hitMe();'>Hit</button>\
-				<button class='btn btn-lg btn-default' onclick='stand();'>Stand</button>\
+			<div class='btn-group' style='margin-bottom:5px;'>\
+				<button class='btn btn-lg btn-success' onclick='deal();'>Deal</button>\
+				<button class='btn btn-lg btn-primary' onclick='hitMe();'>Hit</button>\
+				<button class='btn btn-lg btn-warning' onclick='stand();'>Stand</button>\
 			</div>\
 		</div>");
+		deck = cards;
+		playerHand = new Array();
+		dealerHand = new Array();
+		dealerHand = 0;
+		playerHand = 0;
+		updateCount(false); //update player points
+	}
 }
 
 //Handles dealing two cards to each player
@@ -80,10 +90,11 @@ function dealCard(name, cardObj) {
 		dealerCards.push(cardObj);
 		dealerHand++;
 	}
+	updateCount(false);
 }
 
 function hitMe() {
-
+	dealCard("player", getNewCard());
 }
 
 function stand() {
@@ -101,8 +112,8 @@ function getNewCard() {
 	else return false;
 }
 
-//Updates the number of points each player has
-function updateCount() {
+//Updates the number of points player has. if updateDealer is true, dealer is also updated
+function updateCount(updateDealer) {
 	var dealerDiv = $("#dealerInfo");
 	var playerDiv = $("#playerInfo");
 	var dealerPoints = 0;
@@ -113,6 +124,13 @@ function updateCount() {
 	for(var i = 0; i < playerCards.length; i++) {
 		playerPoints += playerCards[i].points;
 	}
-	dealerDiv.empty().append("<h3>" + dealerPoints + "</h3>");
+	if(updateDealer) dealerDiv.empty().append("<h3>" + dealerPoints + "</h3>");
 	playerDiv.empty().append("<h3>" + playerPoints + "</h3>");
+}
+
+//Handles logic for dealer getting cards
+function dealer() {
+	while(dealerPoints < 17) {
+		deal("dealer", getNewCard());
+	}
 }
